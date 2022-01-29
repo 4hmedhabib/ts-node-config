7d8c4803173f12cb5a81639440146c6e86b14794
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-01-29 20:44:49 +0300

    added ts syntax

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 const app = express();
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("ok");
+interface Cal {
+  x: number;
+  y: number;
+}
+
+app.get("/add", (req: Request, res: Response, next: NextFunction) => {
+  const addFunc = (x: number, y: number) => {
+    return x + y;
+  };
+
+  res.send(addFunc(2, 5));
 });
 
 app.listen(PORT, () => {
